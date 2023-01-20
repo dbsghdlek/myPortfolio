912c91e74bb3fa91d366cce2997116fbedafd55b
yoon ho <dbsghdlek2@naver.com>
2023-01-20 19:54:59 +0900

    테스트 커밋

--- portfolio/portfolio/src/main/java/com/personal/portfolio/repository/AniRepository.java
+++ portfolio/portfolio/src/main/java/com/personal/portfolio/repository/AniRepository.java
@@ -1,10 +1,7 @@
 package com.personal.portfolio.repository;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
 
-@Entity
+
 public class AniRepository {
-    @Id
-    private int id;
+
 }
