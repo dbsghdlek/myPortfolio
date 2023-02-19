880482234f986bf1830adab977d7cfe03aa64db0
yoon ho <dbsghdlek2@naver.com>
2023-02-19 23:01:45 +0900

    Template 분할

--- portfolio/portfolio/src/main/java/com/personal/portfolio/repository/AniRepository.java
+++ portfolio/portfolio/src/main/java/com/personal/portfolio/repository/AniRepository.java
@@ -1,7 +1,5 @@
 package com.personal.portfolio.repository;
 
-
-
 public class AniRepository {
 
 }
---
+++ portfolio_vue/src/components/main_portfolio.vue
@@ -0,0 +1,45 @@
+<template>
+    <section class="page-section portfolio" id="portfolio">
+        <div class="container">
+            <!-- Portfolio Section Heading-->
+            <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Portfolio</h2>
+            <!-- Icon Divider-->
+            <div class="divider-custom">
+                <div class="divider-custom-line"></div>
+                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                <div class="divider-custom-line"></div>
+            </div>
+            <!-- Portfolio Grid Items-->
+            <div :v-for="item_image in images" lass="row justify-content-center" id="items">
+                <!-- Portfolio Item 1-->
+                <div class="col-md-6 col-lg-4 mb-5">
+                    <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+                        <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                            <div class="portfolio-item-caption-content text-center text-white"><i
+                                    class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="../assets/img/portfolio/{{item_image}}.png" alt="..." />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script> 
+new Vue({
+    el : "#items",
+    data : {
+        images : ["ani","game","puppy","food","travel","music"]
+    },
+    methods: {
+        
+    }
+})
+
+export default {
+}
+</script>
+
+<style>
+
+</style>
