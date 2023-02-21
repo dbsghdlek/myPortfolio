c2ea4dd3551ba343d95d5711f1e20bd064522183
yoon ho <dbsghdlek2@naver.com>
2023-02-21 23:00:45 +0900

    테스트 api 생성

--- portfolio/portfolio/src/main/java/com/personal/portfolio/domain/repository/PuppyRepository.java
+++ portfolio/portfolio/src/main/java/com/personal/portfolio/domain/repository/PuppyRepository.java
@@ -1,6 +1,5 @@
 package com.personal.portfolio.repository;
 
-import  javax.persistence.*;
 public class PuppyRepository {
 
 }
---
+++ portfolio/portfolio/src/main/java/com/personal/portfolio/web/controller/MenuController.java
@@ -0,0 +1,2 @@
+package com.personal.portfolio.web.controller;public class MenuController {
+}
