efdd808f6e27bcd4233d3050283e3527e3ec2fbc
yoonho <dbsghdlek2@naver.com>
2023-06-03 00:23:17 +0900

    빌더 오류 수정

--- portfolio/portfolio/src/main/java/com/personal/portfolio/web/service/MenuService.java
+++ portfolio/portfolio/src/main/java/com/personal/portfolio/web/service/MenuService.java
@@ -25,35 +25,52 @@ public class MenuService {
         return menuMapper.getAllUser();
     }
 
+    //메뉴 Entity 리스트를 Dto 리스트로 변환해주는 메서드
+    public List<MenuDto> listEntityToDto(List<MenuEntity> list){
+
+        List<MenuDto> dtoList = new ArrayList<MenuDto>();
+
+        for(MenuEntity menuEntity : list){
+            MenuDto menu = MenuDto.builder()
+                    .menuId(menuEntity.getMenuId())
+                    .menuName(menuEntity.getMenuName())
+                    .createDate(menuEntity.getCreateDate())
+                    .build();
+
+            dtoList.add(menu);
+        }
+
+        return  dtoList;
+    };
+
+    // Entity를 Dto로 변경
+    public MenuDto entityToDto(MenuEntity menuEntity){
+        if(menuEntity != null){
+            return MenuDto.builder()
+                    .menuId(menuEntity.getMenuId())
+                    .menuName(menuEntity.getMenuName())
+                    .createDate(menuEntity.getCreateDate())
+                    .build();
+        }else{
+            return null;
+        }
+    }
+
     // * 메뉴 리스트 가져오기 */
     public List<MenuDto> allMenuByJpa(){
         List<MenuEntity> Entitylist =menuRepository.findAll();
-        List<MenuDto> dtoList = new ArrayList<MenuDto>();
-        for(MenuEntity menuEntity : Entitylist){
-            MenuDto menu = new MenuDto();
-            menu.builder()
-                    .menuId(menuEntity.getMenuId())
-                    .menuName(menuEntity.getMenuName())
-                    .createDate(menuEntity.getCreateDate());
-            dtoList.add(menu);
-        }
-        return  dtoList;
+
+        List<MenuDto> menuList = listEntityToDto(Entitylist);
+
+        return menuList;
     }
 
     //단일 메뉴 정보 가져오기
     public MenuDto getMenu(int menuId){
         Optional<MenuEntity> optionalMenu = menuRepository.findById(menuId);
         MenuEntity menuEntity = optionalMenu.orElse(null);
-        MenuDto menuDto = new MenuDto();
-        if(menuEntity != null){
-            menuDto.builder()
-                    .menuId(menuEntity.getMenuId())
-                    .menuName(menuEntity.getMenuName())
-                    .createDate(menuEntity.getCreateDate());
-            log.info("menuDto Output [{}, {}, {}]", menuDto.getMenuId(), menuDto.getMenuName(), menuDto.getCreateDate());
-        }else{
-            menuDto = null;
-        }
+        MenuDto menuDto = entityToDto(menuEntity);
+
         return menuDto;
     }
 }
