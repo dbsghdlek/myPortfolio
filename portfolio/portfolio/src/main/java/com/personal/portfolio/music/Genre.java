9a42d1044fb2dfe2527d728f93872a089c01aecd
Yoonho Kim <dbsghdlek2@naver.com>
2024-05-11 23:32:05 +0900

    entity명 변경

--- portfolio/portfolio/src/main/java/com/personal/portfolio/music/Genre.java
+++
@@ -1,9 +0,0 @@
-package com.personal.portfolio.music;
-
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public enum Genre {
-    KPOP("Kpop"),JPOP("Jpop"),POP("pop");
-    private String genreName;
-}
