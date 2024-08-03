546c80d95b231b484182819bf82007419b5c2465
Yoonho Kim <dbsghdlek2@naver.com>
2024-08-03 23:43:35 +0900

    24/08/03 JWT 로직 추가

---
+++ portfolio/src/main/java/com/example/portfolio/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.example.portfolio.config;
+
+import com.example.portfolio.config.jwt.JwtAccessDeniedHandler;
+import com.example.portfolio.config.jwt.JwtAuthenticationEntryPoint;
+import com.example.portfolio.config.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return web -> web.ignoring().requestMatchers("/favicon.ico");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable())
+
+                .exceptionHandling(authenticcationManager -> authenticcationManager
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler))
+                .headers(Customizer.withDefaults())
+                .authorizeRequests()
+                .requestMatchers(new AntPathRequestMatcher("/admin/**")).hasAnyRole("ADMIN")
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+}
--- portfolio/src/main/java/com/example/portfolio/config/filter/security/SecurityConfig.java
+++
@@ -1,4 +0,0 @@
-package com.example.portfolio.config.filter.security;
-
-public class SecurityConfig {
-}
