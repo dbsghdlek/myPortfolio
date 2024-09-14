14a9f12d0bc378e11e1a459ed1912b414ba44d3e
Yoonho Kim <dbsghdlek2@naver.com>
2024-09-14 22:50:30 +0900

    update

--- portfolio/src/main/java/com/example/portfolio/domain/user/UserController.java
+++ portfolio/src/main/java/com/example/portfolio/domain/user/UserController.java
@@ -15,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -25,7 +26,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
--- portfolio/src/main/java/com/example/portfolio/domain/user/service/AdminUserService.java
+++ portfolio/src/main/java/com/example/portfolio/domain/user/service/AdminUserService.java
@@ -1,4 +1,7 @@
 package com.example.portfolio.domain.user.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AdminUserService {
 }
